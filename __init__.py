bf151c8468736cd97d69e607ff8b44366c749714
thegreathoe <thegreathoe@gmail.com>
2017-06-28 17:49:48 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,99 +1,28 @@
 # -*- coding: utf-8 -*-
-import os
-from subprocess import Popen, PIPE, call
-
-from modules import cbpi, app
+from modules import cbpi
 from modules.core.hardware import SensorPassive
-import json
-import os, re, threading, time
-from flask import Blueprint, render_template, request
 from modules.core.props import Property
-
 import max31865
-csPin = 8
-misoPin = 9
-mosiPin = 10
-clkPin = 11
-max = max31865.max31865(csPin,misoPin,mosiPin,clkPin)
-
-blueprint = Blueprint('PT100', __name__)
-
-temp = 22
-
-class myThread (threading.Thread):
-
-    value = 0
-
-
-    def __init__(self, sensor_name):
-        threading.Thread.__init__(self)
-        self.value = 0
-        self.sensor_name = sensor_name
-        self.runnig = True
-
-    def shutdown(self):
-        pass
-
-    def stop(self):
-        self.runnig = False
-
-    def run(self):
-
-        while self.runnig:
-            try:
-                app.logger.info("READ TEMP")
-                ##### read temp from max
-                self.value = max.readTemp()
-            except:
-                pass
-
-            time.sleep(4)
-
-
 
 @cbpi.sensor
 class PT100(SensorPassive):
-
-    sensor_name = Property.Text("SensorPT100")
-
+    # CONFIG PARAMETER	
+    csPin  = Property.Select("csPin", options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
+    misoPin = 9
+    mosiPin = 10
+    clkPin = 11
+    
     def init(self):
 
-        self.t = myThread(self.sensor_name)
-
-        def shudown():
-            shudown.cb.shutdown()
-        shudown.cb = self.t
-
-        self.t.start()
-
-    def stop(self):
-        try:
-            self.t.stop()
-        except:
-            pass
+        # INIT SENSOR
+        self.max = max31865.max31865(int(self.csPin),int(self.misoPin), int(self.mosiPin), int(self.clkPin))
 
     def read(self):
+
+        # READ SENSOR
         if self.get_config_parameter("unit", "C") == "C":
-            self.data_received(round(self.t.value, 2))
+            self.data_received(round(self.max.readTemp(), 2))
         else:
-            self.data_received(round(9.0 / 5.0 * self.t.value + 32, 2))
-
-    @classmethod
-    def init_global(cls):
-        '''
-        Called one at the startup for all sensors
-        :return: 
-        '''
+            self.data_received(round(9.0 / 5.0 * self.max.readTemp() + 32, 2))
 
 
-@blueprint.route('/<int:t>', methods=['GET'])
-def set_temp(t):
-    global temp
-    temp = t
-    return ('', 204)
-
-
-@cbpi.initalizer()
-def init(cbpi):
-    cbpi.app.logger.info("INITIALIZE PT100 MODULE")
-    cbpi.app.register_blueprint(blueprint, url_prefix='/api/pt100')
